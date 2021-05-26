e3c407aa6149535ff50bc86379e6e698843dafab
Vlad <vladdp70@gmail.com>
2021-05-27 00:23:21 +0300

    Нормальный стек

--- Stack.js
+++ Stack.js
@@ -1,6 +1,6 @@
 class Stack {
-  constructor(maxSize, ...array) {
-    this.maxSize = maxSize;
+  constructor(maxSize = 10, ...array) {
+    this._maxSize = maxSize;
     this._size = 0;
 
     for (const item of array) {
@@ -17,25 +17,36 @@ class Stack {
   }
 
   push(value) {
-    if (this.size >= this.maxSize) {
-      throw new RangeError("S o");
+    if (this.size >= this._maxSize) {
+      throw new RangeError('Stack overflow');
     }
     this[`_${this.size}`] = value;
     this._size++;
     return this.size;
   }
-  // pop() {
-  //   if (this.size <= 0) {
-  //     return;
-  //   }
-  //   const lastItem = this[`_${this.size - 1}`];
-  //   delete this[`_${this.size - 1}`];
-  //   this._size--;
-  //   return lastItem;
-  // }
-  // pick() {
-  //   return this[`_${this.size - 1}`];
-  // }
+
+  pop() {
+    if (this.size <= 0) {
+      return;
+    }
+    const lastItem = this[`_${this.size - 1}`];
+    delete this[`_${this.size - 1}`];
+    this._size--;
+    return lastItem;
+  }
+
+  pick() {
+    return this[`_${this.size - 1}`];
+  }
 }
 
-const newStack = new Stack(10, 12, 13, 14, 15);
+const newStack = new Stack(5, 12, 13, 14, 15);
+
+const checkSequence = (str) => {
+  const stack = new Stack();
+
+  for (const symbol of str) {
+  }
+
+  return stack.isEmpty;
+};
