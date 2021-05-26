6396b5ac9720ef9affdba33298ed161fec83b291
Vlad <vladdp70@gmail.com>
2021-05-27 01:43:09 +0300

    Сделаны задания на очередь

--- Queue.js
+++ Queue.js
@@ -1,7 +1,11 @@
 class Queue {
-  constructor() {
+  constructor(...args) {
     this._head = 0;
     this._tail = 0;
+
+    for (const item of args) {
+      this.push(item);
+    }
   }
 
   get size() {
@@ -14,12 +18,32 @@ class Queue {
     return this.size;
   }
   pop() {
-    if(this.size){
+    if (this.size) {
       const lastItem = this[this._head];
-    delete this[this._head++];
-    return lastItem;
+      delete this[this._head++];
+      return lastItem;
     }
-    
   }
 }
-const q = new Queue();
+const q1 = new Queue(1, 3, 5, 7, 11, 13, 15);
+const q2 = new Queue(2, 4, 6, 8);
+/*
+mergeQueues(q1,q2);
+Функция, котрая сливает две очереди в одну.
+Элементы из очередей становятся в новую очередь поочередно
+*/
+
+const mergeQueues = (q1, q2) => {
+  const result = new Queue();
+
+  while (q1.size || q2.size) {
+    if (q1.size) {
+      result.push(q1.pop());
+    }
+
+    if (q2.size) {
+      result.push(q2.pop());
+    }
+  }
+  return result;
+};
